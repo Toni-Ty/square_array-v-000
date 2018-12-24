c8791aa44c0c94550924b32e9e92e6256f428c05
Toni-Ty <antonique2479@yahoo.com>
2018-12-24 07:27:38 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,10 @@
-def square_array(array)
-  square = array.each {|num| num ** 2}
-    puts square
-   end
+# def square_array(array)
+#   square = array.each {|num| num ** 2}
+#     puts square
+#    end
+
+   def square_array(array)
+  new_array = []
+  array.each{|a| new_array.push(a*a)}
+  return new_array
+end
